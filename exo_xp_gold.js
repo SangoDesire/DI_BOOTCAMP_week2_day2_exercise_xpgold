33f9a8534b9d5a067bcd8fb74b35ac927eab364a
analysteprodez <analysteprodez@gmail.com>
2022-12-28 19:40:23 +0000

    week2 day2 exerciceXP

---
+++ exo_xp_gold.js
@@ -0,0 +1,14 @@
+let langage = prompt("which language do you  speak ?");
+let convert = langage.toLowerCase
+switch (langage = "french") {
+    alert("Bonjour ");
+}
+case x:
+    // code block
+    break;
+case y:
+    // code block
+    break;
+default:
+    // code block
+    }
